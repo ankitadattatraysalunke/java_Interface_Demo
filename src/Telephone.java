55fd4a91fdc917c90007bc2012bfda3bc92cb019
ankitadattatraysalunke <ankita.dattatray.salunke@gmail.com>
2024-05-16 18:28:04 +0530

    Git Telecommuncation app

---
+++ src/Telephone.java
@@ -0,0 +1,14 @@
+public interface Telephone {
+    //power on
+    //dial a number
+    //answer phone call
+    //call another phone
+    //is ringing
+
+    void powerOn();
+    void dial(String phoneNumber);
+    void answer();
+    boolean callPhone(String phoneNumber);
+    boolean isRinging();
+
+}
